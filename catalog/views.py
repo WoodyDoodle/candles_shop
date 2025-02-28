7b13391c1945a3b66e04c05695376e7047caa0ba
Dmitriy K <krutikow24@gmail.com>
2025-02-28 15:39:39 +0500

    working ad visual interface (templates)

--- catalog/views.py
+++ catalog/views.py
@@ -8,14 +8,19 @@ def index(request):
 
 def catalog(request):
     products = Product.objects.all()
-    context = {'products': products}
+    context = {
+        'products': products,
+        'current_page': 'shop',
+               }
     return render(request, 'html/shop.html', context)
 
 def about(request):
-    return render(request, 'html/about.html')
+    context = {'current_page': 'about'}
+    return render(request, 'html/about.html', context)
 
 def contact(request):
-    return render(request, 'html/contact.html')
+    context = {'current_page': 'contact'}
+    return render(request, 'html/contact.html', context)
 
 def products(request):
     return render(request, 'html/products.html')
